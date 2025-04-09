89b7bea389f64aa803d081d8ec0d6cb9b4f97f2d
Krishna-69 <Krishnarastogi67@gmail.com>
2025-04-09 16:52:51 +0530

    .

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,19 +1,17 @@
+// app/board/[boardId]/page.tsx
+
 import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 
-interface BoardIdPageProps {
-    params: {
-        boardId: string;
-    };
+export default async function BoardIdPage({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 }
-
-const BoardIdPage =async ({ params }: BoardIdPageProps) => {
-    return (
-        <Room roomId={params.boardId} fallback={<Loading />}>
-            <Canvas boardId={params.boardId} />
-        </Room>
-    );
-};
-
-export default BoardIdPage;
