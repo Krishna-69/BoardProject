9767dc0a0735f1da4957ac00245d6a7dacd8357f
Krishna Rastogi <krishnarastogi67@gmail.com>
2025-07-13 18:01:27 +0530

    Update page.tsx

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -4,11 +4,13 @@ import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 
-export default async function BoardIdPage({
-  params,
-}: {
-  params: { boardId: string };
-}) {
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+export default async function BoardIdPage({ params }: BoardIdPageProps) {
   return (
     <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
